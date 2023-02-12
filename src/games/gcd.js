dda0c1b3f888c049e62bbd240d5dbae5cb45027e
Ildar Nazipov <ildar.nazipov@gmail.com>
2023-02-12 23:06:06 +0300

    step 7

---
+++ src/games/gcd.js
@@ -0,0 +1,27 @@
+import playGame from '../index.js';
+
+const playBrainGcd = () => {
+  const getQuestionBrainGcd = () => {
+    const randomleftNumber = Math.floor(Math.random() * 100);
+    const randomRightNumber = Math.floor(Math.random() * 100);
+    return `${randomleftNumber} ${randomRightNumber}`;
+  };
+
+  const getCorrectAnswerBrainGcd = (question) => {
+    const [left, right] = question.split(' ');
+    let divisor;
+    if (Number(left) >= Number(right)) {
+      divisor = Number(right);
+    } else {
+      divisor = Number(left);
+    }
+    while (Number(left) % divisor !== 0 || Number(right) % divisor !== 0) {
+      divisor -= 1;
+    }
+    return divisor.toString();
+  };
+
+  playGame('Find the greatest common divisor of given numbers.', getQuestionBrainGcd, getCorrectAnswerBrainGcd);
+};
+
+export default playBrainGcd;
